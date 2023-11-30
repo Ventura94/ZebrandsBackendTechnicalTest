c4c568cc4f0c17cc05dfaa6729bb3e1b4c84084f
Arian Ventura Rodríguez <aventura@dataint.mx>
2023-11-30 16:02:20 -0500

    product migration was carried out

--- src/alembic/versions/2023-11-30_first_migration.py
+++ src/alembic/versions/2023-11-30_first_migration.py
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '3bb35615b9bf'
+revision = "3bb35615b9bf"
 down_revision = None
 branch_labels = None
 depends_on = None
