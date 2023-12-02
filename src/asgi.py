7b2279844da870b47b94aa0f93d4b136056749bd
Arian Ventura Rodríguez <aventura@dataint.mx>
2023-12-02 15:44:01 -0500

    change cors middleware and AUD environment var

--- src/asgi.py
+++ src/asgi.py
@@ -33,7 +33,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://zebrands.alicfornia.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
